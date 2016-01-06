ad1459e3c36dd2b5f73f2fd3449573042ab89663
neela11 <neelambika11@gmail.com>
2016-01-06 00:25:00 -0800

    new image

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@ $(document).ready(function () {
   ==============================================*/
     function initialize() {
         var map;
-        var location1 = new google.maps.LatLng('43.306599', '17.821524');
+        var location1 = new google.maps.LatLng('47.8038421', '-122.211613');
+
         var mapOptions = {
-            draggable: false,
+            draggable: true,
             // How zoomed in you want the map to start at (always required)
-            zoom: 5,
+            zoom: 3,
             disableDefaultUI: false,
             center: location1,
-            scrollwheel: false,
+            scrollwheel: true,
             styles: [{ featureType: "landscape", stylers: [{ saturation: -100 }, { lightness: 65 }, { visibility: "on" }] }, { featureType: "poi", stylers: [{ saturation: -100 }, { lightness: 51 }, { visibility: "simplified" }] }, { featureType: "road.highway", stylers: [{ saturation: -100 }, { visibility: "simplified" }] }, { featureType: "road.arterial", stylers: [{ saturation: -100 }, { lightness: 30 }, { visibility: "on" }] }, { featureType: "road.local", stylers: [{ saturation: -100 }, { lightness: 40 }, { visibility: "on" }] }, { featureType: "transit", stylers: [{ saturation: -100 }, { visibility: "simplified" }] }, { featureType: "administrative.province", stylers: [{ visibility: "off" }]/**/ }, { featureType: "administrative.locality", stylers: [{ visibility: "off" }] }, { featureType: "administrative.neighborhood", stylers: [{ visibility: "on" }]/**/ }, { featureType: "water", elementType: "labels", stylers: [{ visibility: "on" }, { lightness: -25 }, { saturation: -100 }] }, { featureType: "water", elementType: "geometry", stylers: [{ hue: "#ffff00" }, { lightness: +10 }, { saturation: -97 }] }]
         };
         map = new google.maps.Map(document.getElementById('map-container'),
